bfc49f1a000964bbd354b6d66a2304d668db46ae
Sergey <serzh.tsvetckoff2012@yandex.ru>
2021-09-20 20:47:05 +0300

    Доделать дз_2

--- src/Lesson01/online/HomeWork2/HomeWork2.java
+++ src/Lesson01/online/HomeWork2/HomeWork2.java
@@ -1,7 +1,10 @@
 package HomeWork2;
 
 public class HomeWork2 {
-    public static void main(String[] args) {
-        
+    public static main (String[] args) {
+        }
+   public static boolean within10and20( int a, int b) {
+        return 9> a + b && a + b <21;
+   }
     }
-}
+
