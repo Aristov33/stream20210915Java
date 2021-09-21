0bd473c2f2f5b553de4209d9d5cf283978009d2e
Sergey <serzh.tsvetckoff2012@yandex.ru>
2021-09-21 13:10:43 +0300

    ДЗ_2 готово

--- src/Lesson01/online/HomeWork2/HomeWork2.java
+++ src/Lesson01/online/HomeWork2/HomeWork2.java
@@ -1,10 +1,40 @@
 package HomeWork2;
 
 public class HomeWork2 {
-    public static main (String[] args) {
-        }
-   public static boolean within10and20( int a, int b) {
-        return 9> a + b && a + b <21;
-   }
+    public static void main(String[] args) {
+        System.out.println(taskone(10, 10));
+        tasktwo(-4);
+        System.out.println(taskthree(0));
+        printWordNTimes("Сергей" , 4);
     }
+
+    public static boolean taskone(int a, int b) {
+        System.out.println("Задание 3.");
+        int c = a + b;
+        if (c > 9 && c < 21) return true;
+        else return false;
+    }
+
+    public static void tasktwo(int a) {
+        if (a >= 0) {
+            System.out.println("Положительное");
+        } else {
+            System.out.println("Отрицательное");
+
+        }
+    }
+
+    public static boolean taskthree(int a) {
+        if (a < 0) return true;
+        else return false;
+    }
+
+    public static void printWordNTimes(String word, int times) {
+        for (int k =1; k < times; k++) {
+            System.out.println("[ " + k + " ]" + " "+word);
+        }
+    }
+}
+
+
 
