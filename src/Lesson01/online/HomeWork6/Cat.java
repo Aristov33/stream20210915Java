daa21dac85b5a76b593e9a7250351e6948e91796
Sergey <serzh.tsvetckoff2012@yandex.ru>
2021-10-07 16:38:22 +0300

    .cat

--- src/Lesson01/online/HomeWork6/Cat.java
+++ src/Lesson01/online/HomeWork6/Cat.java
@@ -5,18 +5,18 @@ public class Cat extends Animal{
     private final double MAX_JUMP_HEIGTH = 2;
     @Override
     void run(int length) {
-        if ((length >= 0) && (length <= MAX_RUN_LENGTH)) System.out.println("run: true");
-        else System.out.println("run: false");
+        if ((length >= 0) && (length <= MAX_RUN_LENGTH)) System.out.println("run: " + length);
+        else System.out.println("run: Неверное действие");
     }
 
     @Override
     void swim(int length) {
-        System.out.println("swim: false");
+        System.out.println("swim: Неверное действие");
     }
 
     @Override
     void jump(double height) {
-        if ((height >= 0) && (height <= MAX_JUMP_HEIGTH)) System.out.println("jump: true");
-        else System.out.println("jump: fase");
+        if ((height >= 0) && (height <= MAX_JUMP_HEIGTH)) System.out.println("jump: "+length );
+        else System.out.println("jump: Неверное действие");
     }
 }
