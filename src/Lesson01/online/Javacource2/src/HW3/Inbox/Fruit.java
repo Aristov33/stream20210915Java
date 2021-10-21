0288c5b78725ea049e367832d06840d9b023e387
Sergey <serzh.tsvetckoff2012@yandex.ru>
2021-10-21 11:53:44 +0300

    ДЗ-3

---
+++ src/Lesson01/online/Javacource2/src/HW3/Inbox/Fruit.java
@@ -0,0 +1,14 @@
+package HW3.Inbox;
+
+public abstract class Fruit {
+
+    private float weight;
+
+    public Fruit(float weight) {
+        this.weight = weight;
+    }
+
+    float getWeight() {
+        return weight;
+    }
+}
---
+++ src/Lesson01/online/Javacource2/src/HW3/Inbox/Orange.java
@@ -0,0 +1,7 @@
+package HW3.Inbox;
+
+public class Orange extends Fruit{
+    public Orange() {
+        super(1.5f);
+    }
+}
